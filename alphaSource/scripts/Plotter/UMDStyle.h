52974f2503cf9c62a00fd918d04e3c9701728d23
chrispap95 <chrispap95@gmail.com>
2018-10-29 13:45:01 -0400

    Fixed symlinks to real files

--- alphaSource/scripts/Plotter/UMDStyle.h
+++ alphaSource/scripts/Plotter/UMDStyle.h
@@ -1,1 +1,16 @@
-/Users/jengbou/UMDStyle.h
+//
+//   @file    UMDStyle.h         
+//   
+//            UMD Style, based on a style file from BaBar
+//
+
+#ifndef  __UMDSTYLE_H
+#define __UMDSTYLE_H
+
+#include "TStyle.h"
+
+void SetUMDStyle();
+
+TStyle* UMDStyle(); 
+
+#endif // __UMDSTYLE_H
