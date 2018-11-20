3d354b5808b0ed6bc45b0ac854f5539456e366b9
Christos Papageorgakis <chrispap95@gmail.com>
2018-11-20 00:57:45 -0500

    Converting NIST script to GIF++

--- alphaSource/doseConstant.py
+++ alphaSource/doseConstant.py
@@ -1,31 +1,15 @@
 #!/usr/bin/python
-
-###################################################################################################
-##                                                                                               ##
-##   This routine is going to fit one measurement and give the peak and the stat+syst error      ##
-##                                                                                               ##
-###################################################################################################
-
-# Import some python libraries
 import os, sys, math, datetime
-
-# Import ROOT
 from ROOT import gROOT, gStyle, TFile, TTree, TH1F, TH1D, TCanvas, TPad, TMath, TF1, TLegend, gPad, gDirectory
-from ROOT import kRed, kBlue, kGreen, kWhite
 
-# Some more python libraries
 from collections import OrderedDict
 import numpy
 
-# Pass the current working directory to the compiler
 sys.path.append(os.path.abspath(os.path.curdir))
 
-# Import some custom styling methods
 from Plotter import parseLYAnaInputArgs
+from Plotter.CommonTools import DrawHistSimple, DrawDvsTHist
 options = parseLYAnaInputArgs()
-
-# Import fitter
-from Plotter.CommonTools import DrawDvsTHist
 
 gROOT.LoadMacro("Plotter/UMDStyle.C")
 from ROOT import SetUMDStyle
@@ -35,37 +19,102 @@ gROOT.SetBatch()
 ####################################################################################################
 ####################################################################################################
 if __name__ == '__main__':
+    print options
+
     myfile = {}
 
+    ## binbase, xmin, xmax : nbins = binbase*(xmax-xmin)
     hxrng = {}
-    hxrng["Ref"] = [32,-1,8]
+    hxrng["Ref"]   = [32,-1,8]
     hxrng["EJ200"] = [32,-1,6]
     hxrng["EJ260"] = [32,-0.5,4]
+
+    hxrng["T1"] = [32,-1,8.0]
+    hxrng["T2"] = [32,-1,8.5]
+    hxrng["T3"] = [32,-1,9.0]
+    hxrng["T4"] = [33,-1,9.5]
 
     today = datetime.date.today()
     fTag = today.strftime("%Y%m%d")
 
     ## Output directory
-    outDir = "Results/20181119"
+    outDir = "Results/GIF++"
     try:
         os.makedirs(outDir)
     except:
         pass
 
-    ## NIST set1 ## It's meaningless right now
-    doselabel = "7 Mrad@ 390 krad/hr; 112 days aft. irr."
+    ## NIST setV3
+    sampleSet = "NISTV3"
+    doselabel = "7 Mrad@ 8.19 krad/hr"
 
-    ## 10/11/18
-    myfile["EJ200PVT_1X1P_N8-20181011"] = ["Ref",TFile("root/AlphaSource/Pu239new_EJ200-2X_3_Default_Nofoil_FaceA_FastFrame_20181011.root")]
+    ## Un-irr samples
+    myfile["EJ200PVT_T1_1-20161103"] = ["T1",TFile("root/AlphaSource/Pu239new_EJ200PVT-T1_1_Default_Nofoil_FastFrame_20161103_merged.root")]
+    myfile["EJ200PVT_T1_1-20161108"] = ["T1",TFile("root/AlphaSource/Pu239new_EJ200PVT-T1_1_Default_Nofoil_FastFrame_20161108_merged.root")]
+    ## Un-irr samples
+    myfile["EJ200PVT_T2_1-20161103"] = ["T2",TFile("root/AlphaSource/Pu239new_EJ200PVT-T2_1_Default_Nofoil_FastFrame_20161103_merged.root")]
+    myfile["EJ200PVT_T2_1-20161108"] = ["T2",TFile("root/AlphaSource/Pu239new_EJ200PVT-T2_1_Default_Nofoil_FastFrame_20161108_merged.root")]
+    ## Un-irr samples
+    myfile["EJ200PVT_T3_1-20161103"] = ["T3",TFile("root/AlphaSource/Pu239new_EJ200PVT-T3_1_Default_Nofoil_FastFrame_20161103_merged.root")]
+    myfile["EJ200PVT_T3_1-20161108"] = ["T3",TFile("root/AlphaSource/Pu239new_EJ200PVT-T3_1_Default_Nofoil_FastFrame_20161108_merged.root")]
+    ## Un-irr samples
+    myfile["EJ200PVT_T4_1-20161103"] = ["T4",TFile("root/AlphaSource/Pu239new_EJ200PVT-T4_1_Default_Nofoil_FastFrame_20161103_merged.root")]
+    myfile["EJ200PVT_T4_1-20161108"] = ["T4",TFile("root/AlphaSource/Pu239new_EJ200PVT-T4_1_Default_Nofoil_FastFrame_20161108_merged.root")]
 
-    ## 10/12/18
-    myfile["EJ200PVT_1X1P_N1-20181012"] = ["Ref",TFile("root/AlphaSource/Pu239new_EJ200-2X_3_Default_Nofoil_FaceA_FastFrame_20181012.root")]
+    ## [fitrng0,fitrng1,fitterType,biasOffset]
+    ## [1.3,1.1,"G2",0.3112]
+
+    myfile["EJ200PVT_T1_1-UnIrr"] = [
+        "T1",TFile("root/AlphaSource/Pu239new_EJ200PVT-T1_1_Default_Nofoil_FastFrame_20161108_merged.root"),[1.3,1.1,"G2",0.]]
+    myfile["EJ200PVT_T1_2-001d"]  = [
+        "T1",TFile("root/AlphaSource/Pu239new_EJ200PVT-T1_2_Default_Nofoil_FastFrame_20170302_merged.root"),[1.1,1.1,"G1",0.3112]]
+    myfile["EJ200PVT_T1_2-028d"]  = [
+        "T1",TFile("root/AlphaSource/Pu239new_EJ200PVT-T1_2_Default_Nofoil_FastFrame_20170329_merged.root"),[1.1,1.1,"G2",0.3112]]
+    myfile["EJ200PVT_T1_2-063d"]  = [
+        "T1",TFile("root/AlphaSource/Pu239new_EJ200PVT-T1_2_Default_Nofoil_FastFrame_20170503_merged.root"),[1.1,1.1,"G2",0.3112]]
+
+    myfile["EJ200PVT_T2_1-UnIrr"] = [
+        "T2",TFile("root/AlphaSource/Pu239new_EJ200PVT-T2_1_Default_Nofoil_FastFrame_20161108_merged.root"),[1.35,1.1,"G2",0.]]
+    myfile["EJ200PVT_T2_2-001d"]  = [
+        "T2",TFile("root/AlphaSource/Pu239new_EJ200PVT-T2_2_Default_Nofoil_FastFrame_20170302_merged.root"),[1.1,1.1,"G2",0.3112]]
+    myfile["EJ200PVT_T2_2-028d"]  = [
+        "T2",TFile("root/AlphaSource/Pu239new_EJ200PVT-T2_2_Default_Nofoil_FastFrame_20170329_merged.root"),[1.1,1.1,"G2",0.3112]]
+    myfile["EJ200PVT_T2_2-063d"]  = [
+        "T2",TFile("root/AlphaSource/Pu239new_EJ200PVT-T2_2_Default_Nofoil_FastFrame_20170503_merged.root"),[1.1,1.1,"G2",0.3112]]
+
+    myfile["EJ200PVT_T3_1-UnIrr"] = [
+        "T3",TFile("root/AlphaSource/Pu239new_EJ200PVT-T3_1_Default_Nofoil_FastFrame_20161108_merged.root"),[1.4,1.1,"G2",0.]]
+    myfile["EJ200PVT_T3_2-001d"]  = [
+        "T3",TFile("root/AlphaSource/Pu239new_EJ200PVT-T3_2_Default_Nofoil_FastFrame_20170302_merged.root"),[1.1,1.1,"G2",0.3112]]
+    myfile["EJ200PVT_T3_2-028d"]  = [
+        "T3",TFile("root/AlphaSource/Pu239new_EJ200PVT-T3_2_Default_Nofoil_FastFrame_20170329_merged.root"),[1.1,1.1,"G2",0.3112]]
+    myfile["EJ200PVT_T3_2-063d"]  = [
+        "T3",TFile("root/AlphaSource/Pu239new_EJ200PVT-T3_2_Default_Nofoil_FastFrame_20170503_merged.root"),[1.1,1.1,"G2",0.3112]]
+
+    myfile["EJ200PVT_T4_1-UnIrr"] = [
+        "T4",TFile("root/AlphaSource/Pu239new_EJ200PVT-T4_1_Default_Nofoil_FastFrame_20161108_merged.root"),[1.55,1.1,"G2",0.]]
+    myfile["EJ200PVT_T4_2-001d"]  = [
+        "T4",TFile("root/AlphaSource/Pu239new_EJ200PVT-T4_2_Default_Nofoil_FastFrame_20170302_merged.root"),[1.1,1.1,"G2",0.3112]]
+    myfile["EJ200PVT_T4_2-028d"]  = [
+        "T4",TFile("root/AlphaSource/Pu239new_EJ200PVT-T4_2_Default_Nofoil_FastFrame_20170329_merged.root"),[1.1,1.1,"G2",0.3112]]
+    myfile["EJ200PVT_T4_2-063d"]  = [
+        "T4",TFile("root/AlphaSource/Pu239new_EJ200PVT-T4_2_Default_Nofoil_FastFrame_20170503_merged.root"),[1.1,1.1,"G2",0.3112]]
 
     plotSets = {}
-    plotSets['0'] = ["EJ200_2X_03-20181023","EJ200_2X_03-20181024"]
 
-    sampleSet = "EJ200PVT"
-    doselabel = "GIF++"
-    uncreftag = "EJ200PVT"
-    dosescheme = "GIF++"
-    D,sigmaD,R,sigmaR,NPE = DrawDvsTHist(myfile, plotSets, outDir, sampleSet, fTag, doselabel, hxrng, options, uncreftag, dosescheme)
+    plotSets['1'] = ["EJ200PVT_T1_1-UnIrr","EJ200PVT_T1_2-001d","EJ200PVT_T1_2-028d","EJ200PVT_T1_2-063d"]
+    plotSets['2'] = ["EJ200PVT_T2_1-UnIrr","EJ200PVT_T2_2-001d","EJ200PVT_T2_2-028d","EJ200PVT_T2_2-063d"]
+    plotSets['3'] = ["EJ200PVT_T3_1-UnIrr","EJ200PVT_T3_2-001d","EJ200PVT_T3_2-028d","EJ200PVT_T3_2-063d"]
+    plotSets['4'] = ["EJ200PVT_T4_1-UnIrr","EJ200PVT_T4_2-001d","EJ200PVT_T4_2-028d","EJ200PVT_T4_2-063d"]
+
+
+    DrawDvsTHist(myfile, plotSets, outDir, sampleSet, fTag, doselabel, hxrng, options, "EJ200PVT", "NIST4")
+
+
+##    plotSetsA = {}
+##    plotSetsA['1'] = ["EJ200PVT_T1_1-20161103","EJ200PVT_T1_1-20161108"]
+##    plotSetsA['2'] = ["EJ200PVT_T2_1-20161103","EJ200PVT_T2_1-20161108"]
+##    plotSetsA['3'] = ["EJ200PVT_T3_1-20161103","EJ200PVT_T3_1-20161108"]
+##    plotSetsA['4'] = ["EJ200PVT_T4_1-20161103","EJ200PVT_T4_1-20161108"]
+
+##    DrawHistSimple(myfile, plotSetsA, outDir, fTag, doselabel, hxrng, options)
