e739e100e3093b08dc5174f36c1c4a5336c53ef8
Christos Papageorgakis <chrispap95@gmail.com>
2019-01-28 19:49:30 -0500

    The file can handle 2 different ls outputs

--- alphaSource/txt2root.sh
+++ alphaSource/txt2root.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 ls -l Data/AlphaSource | grep -ir $1 | \
-awk '{print substr($10,0,length($10))}' > RunLists/List-AlphaSource-$1.txt
-python makeTree.py - -i RunLists/List-AlphaSource-$1.txt -d Data/AlphaSource
+awk '{print substr($11,0,length($11))}' > RunLists/List-AlphaSource-$1.txt
+if [ -z RunLists/List-AlphaSource-$1.txt ]
+then
+        python makeTree.py - -i RunLists/List-AlphaSource-$1.txt -d Data/AlphaSource
+else
+        ls -l Data/AlphaSource | grep -ir $1 | \
+        awk '{print substr($10,0,length($10))}' > RunLists/List-AlphaSource-$1.txt
+        python makeTree.py - -i RunLists/List-AlphaSource-$1.txt -d Data/AlphaSource
+fi
