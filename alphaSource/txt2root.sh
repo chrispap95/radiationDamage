133c07df7dee94397cc78f91e1ca07b3b4ba4d43
Christos Papageorgakis <chrispap95@gmail.com>
2019-01-30 13:51:17 -0500

    First working version

--- alphaSource/txt2root.sh
+++ alphaSource/txt2root.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-ls -l Data/AlphaSource | grep -ir $1 | \
-awk '{print substr($11,0,length($11))}' > RunLists/List-AlphaSource-$1.txt
-if [ -z RunLists/List-AlphaSource-$1.txt ]
-then
-        python makeTree.py - -i RunLists/List-AlphaSource-$1.txt -d Data/AlphaSource
-else
-        ls -l Data/AlphaSource | grep -ir $1 | \
-        awk '{print substr($10,0,length($10))}' > RunLists/List-AlphaSource-$1.txt
-        python makeTree.py - -i RunLists/List-AlphaSource-$1.txt -d Data/AlphaSource
-fi
+ls -l Data/AlphaSource | grep -i $1 | \
+awk '{print substr($15,0,length($15))}' > RunLists/List-AlphaSource-$1.txt
+((y=15))
+while [ ! -z RunLists/List-AlphaSource-$1.txt ] && [ $y -gt 9 ]
+do
+        ((y=$y-1))
+        ls -l Data/AlphaSource | grep -i $1 | \
+        awk -v x=$y '{print substr($(x),0,length($(x)))}' > RunLists/List-AlphaSource-$1.txt
+done
+python makeTree.py - -i RunLists/List-AlphaSource-$1.txt -d Data/AlphaSource
