99b4f4c8ede474941b0280b9052b6aa7c2616258
Christos Papageorgakis <christos.papageorgakis@cern.ch>
2019-11-01 01:03:26 -0400

    Add new measurements

--- AlphaSource/dcost_vs_ddot.C
+++ AlphaSource/dcost_vs_ddot.C
@@ -48,66 +48,80 @@ void dcost_vs_ddot(TString tagTime="default") {
     // Ideally, this is the ONLY section that needs to be updated whenever a new measurement is available.
 
     vector<measurement> measurements;
-    /*measurements.push_back(
-        {
-            true,
-            {0.22,0.22,0.22},
-            {4.33751,4.27079,4.34031},
-            {0.02*0.1,0.02*0.1,0.02*0.1},
-            {0.14129,0.13446,0.14025},
-            4,34,1.5,"GIF++",1,NULL
-        }
-    );
     measurements.push_back(
         {
-            true,
-            {8.53,8.53,8.34,74.4,74.4,390,390,390},
-            {11.80330,11.85346,11.31170,10.36126,11.76172,9.78101,11.44537,7.89202},
-            {8.53*0.1,8.53*0.1,8.34*0.1,74.4*0.1,74.4*0.1,390*0.1,390*0.1,390*0.1},
-            {11.80330*0.01126,11.85346*0.01118,11.31170*0.0114,10.36126*0.01117,
-                11.76172*0.01125,9.78101*0.0113,11.44537*0.01123,7.89202*0.0118
-            },2,34,1.5,"Co-60 (NIST)",1,NULL
+            false,
+            {0.22*0.92,0.31*0.92,0.31*0.92,0.98*0.92,8.53*0.92,74.4*0.92,390*0.92},
+            {3.31789,5.74305,4.87321,6.78443,12.31375,13.46707,15.89024},
+            {0.22*0.1,0.31*0.1,0.31*0.1,0.98*0.1,8.53*0.008,74.4*0.008,390*0.008},
+            {0.59431,0.80162,0.74794,0.83448,0.57133,0.62179,0.73247},
+            2,33,1.2,"EJ200PVT-1X1P",1,NULL
         }
     );
+
     measurements.push_back(
         {
-            true,
-            {0.98,0.98,0.31,0.31},
-            {7.91476,7.63909,5.61088,5.67994},
-            {0.98*0.1,0.98*0.1,0.31*0.1,0.31*0.1},
-            {7.91476*0.01119,7.63909*0.01116,5.61088*0.01121,5.67994*0.01114},
-            3,34,1.5,"Gamma(GSFC REF)",1,NULL
+            false,
+            {0.22*1.0,0.31*1.0,0.31*1.0,0.98*1.0,8.53*1.0,390*1.0},
+            {3.67532,7.02963,5.71187,7.36742,12.77335,15.34901},
+            {0.22*0.1,0.31*0.1,0.31*0.1,0.98*0.1,8.53*0.008,390*0.008},
+            {0.62754,0.9026,0.82169,0.8698,0.57146,0.6859},
+            2,34,1.2,"EJ260PVT-1X1P",1,NULL
         }
-    );*/
+    );
+
+    measurements.push_back(
+        {
+            false,
+            {0.22*0.96,8.53*0.96,74.4*0.96,390*0.96,0.31*0.96,0.98*0.96,0.31*0.96},
+            {4.40333,12.88039,11.11065,10.61664,7.3069,7.88074,5.56525},
+            {0.22*0.1,8.53*0.008,74.4*0.008,390*0.008,0.31*0.1,0.98*0.1,0.31*0.1},
+            {0.69842,0.59055,0.50843,0.4885,0.92941,0.90251,0.81116},
+            4,33,1.2,"EJ200PS-1X1P",1,NULL
+        }
+    );
+
+    measurements.push_back(
+        {
+            false,
+            {0.22*1.04,8.34*1.04,0.31*1.04,390*1.04,0.98*1.04,0.31*1.04},
+            {4.4927,15.12669,4.68655,13.10698,9.09121,6.43075},
+            {0.22*0.1,8.34*0.008,0.31*0.1,390*0.008,0.98*0.1,0.31*0.1},
+            {0.70351,0.66963,0.71287,0.57875,0.97303,0.88434},
+            4,34,1.2,"EJ260PS-1X1P",1,NULL
+        }
+    );
 
     measurements.push_back(
         {
             true,
-            {8.53,74.4,390},
-            {11.80330 ,10.36126 ,9.78101},
-            {8.53*0.1,74.4*0.1,390*0.1},
-            {11.80330*0.01126,10.36126*0.01117,9.78101*0.0113},
-            2,34,1.5,"Co-60 (NIST)",1,NULL
+            {0.22*0.95,8.34*0.97,390*0.97,0.31*0.95},
+            {4.40622,12.07512,8.40505,6.39462},
+            {0.22*0.1,8.34*0.056,390*0.056,0.31*0.1},
+            {0.69878,0.55584,0.3948,0.85466},
+            4,22,1.2,"EJ200PS-2X1P",1,NULL
         }
     );
+
     measurements.push_back(
         {
             true,
-            {0.98,0.31},
-            {7.91476 ,5.61088},
-            {0.98*0.1,0.31*0.1},
-            {7.91476*0.01119,5.61088*0.01121},
-            3,34,1.5,"Gamma(GSFC REF)",1,NULL
+            {0.22,0.31},
+            {4.3876,8.18087},
+            {0.22*0.1,0.31*0.1},
+            {0.69642,0.99314},
+            2,23,1.2,"EJ260PVT-2X1P",1,NULL
         }
     );
+
     measurements.push_back(
         {
             true,
-            {0.22},
-            {4.33751},
-            {0.02*0.1},
-            {0.14129},
-            4,34,1.5,"GIF++",1,NULL
+            {0.22*1.05,8.34*1.03,390*1.03,0.31*1.05},
+            {3.53379,13.18657,17.29626,6.42334},
+            {0.22*0.1,8.34*0.056,390*0.056,0.31*0.1},
+            {0.61619,0.6076,0.79456,0.85839},
+            2,22,1.2,"EJ200PVT-2X1P",1,NULL
         }
     );
 
@@ -169,17 +183,18 @@ void dcost_vs_ddot(TString tagTime="default") {
     allgraphs->Draw("AP");
     allgraphs->GetXaxis()->SetTitle("Dose rate (krad/hr)");
     allgraphs->GetYaxis()->SetTitle("Dose Constant (Mrad)");
-    allgraphs->GetYaxis()->SetTitleOffset(1.52);
+    allgraphs->GetYaxis()->SetTitleOffset(1.2);
+    allgraphs->GetXaxis()->SetTitleOffset(1.3);
 
     // NEED TO TUNE THESE:
-    allgraphs->SetMinimum(1e-2);
-    allgraphs->SetMaximum(50.0);
-    double xmin = 1.e-2;
+    allgraphs->SetMinimum(2.0);
+    allgraphs->SetMaximum(20.0);
+    double xmin = 5.e-2;
     double xmax = 2.e3;
     allgraphs->GetXaxis()->SetLimits(xmin,xmax);
 
     // Prepare the legend
-    TLegend *legendA = new TLegend(0.57,0.22,0.92,0.42);
+    TLegend *legendA = new TLegend(0.57,0.22,0.89,0.42);
     legendA->SetBorderSize(0);
     legendA->SetFillColor(0);
     for (vector<measurement>::iterator i = measurements.begin();i!=measurements.end();++i)
@@ -207,9 +222,9 @@ void dcost_vs_ddot(TString tagTime="default") {
     TString filename = TString("doserate_");
     filename+=tagTime;
 
-    canv->SaveAs(filename+".png");
-    canv->SaveAs(filename+".pdf");
-    canv->SaveAs(filename+".root");
-    canv->SaveAs(filename+".C");
+    //canv->SaveAs(filename+".png");
+    canv->SaveAs(filename+".eps");
+    //canv->SaveAs(filename+".root");
+    //canv->SaveAs(filename+".C");
 
 }
