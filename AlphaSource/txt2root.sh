6b8dab57bcec6dd60569ecbcb5326a362f183007
Chris Papageorgakis <Christos.Papageorgakis@cern.ch>
2025-07-24 12:59:47 -0400

    Updating script

--- AlphaSource/txt2root.sh
+++ AlphaSource/txt2root.sh
@@ -1,6 +1,28 @@
 #!/bin/sh
-ls -l Data/AlphaSource.nosync | grep -i $1 | \
-awk '{print substr($NF,0,length($NF))}' > RunLists/List-AlphaSource-$1.txt
-python2 makeTree.py - -i RunLists/List-AlphaSource-$1.txt -d Data/AlphaSource.nosync
 
-find root/AlphaSource.nosync -name "*-*" -execdir bash -c 'mv -- "$1" "${1//-/_}"' bash {} \;
+# txt2root.sh
+# This script processes files in the AlphaSource directory, generating a list of files
+# that match a given pattern and then creating a ROOT tree from those files.
+# Usage: ./txt2root.sh <pattern>
+
+# Check if output directory exists, if not create it
+if [ ! -d "root/AlphaSource" ]; then
+    mkdir -p root/AlphaSource
+fi
+
+if [ ! -d "RunLists" ]; then
+    mkdir RunLists
+fi
+
+output_name="List-AlphaSource.txt"
+if [ ! -z "$1" ]; then
+    output_name="List-AlphaSource-$1.txt"
+fi
+
+for ff in Data/AlphaSource/*$1*; do
+    basename $ff >> RunLists/$output_name
+done
+python makeTree.py - -i RunLists/$output_name -d Data/AlphaSource
+
+# Standardize file names by replacing hyphens with underscores
+find root/AlphaSource -name "*-*" -execdir bash -c 'mv -- "$1" "${1//-/_}"' bash {} \;
